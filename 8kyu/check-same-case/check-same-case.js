1b8b8b759d2865ed9bbe8c1e0adbb5f83f0ad7dc
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-05-14 17:30:23 +0300

    Add solution to multiply challenge

---
+++ 8kyu/check-same-case/check-same-case.js
@@ -0,0 +1,8 @@
+function sameCase(a, b) {
+  if (a.toLowerCase() != a.toUpperCase || b.toLowerCase() != b.toUpperCase)
+    return -1;
+  else if (a.toLowerCase() == a && b.toLowerCase == b) return 1;
+}
+
+const isChar1Upper = char1 === char1.toUpperCase();
+const isChar2Upper = char2 === char2.toUpperCase();
